9f17ff43b9796fc5e5a2698c60350edb37d86501
galimre63 <galimre63@gmail.com>
2020-03-12 22:45:20 +0100

    2 files modified, 1 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, LoginComponent ],
+  declarations: [ AppComponent, LoginComponent, MainComponent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
