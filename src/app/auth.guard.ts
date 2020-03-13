610df07fec4dbe885a9036042d7523f15014f924
galimre63 <galimre63@gmail.com>
2020-03-13 15:28:10 +0100

    2 files modified

--- src/app/auth.guard.ts
+++ src/app/auth.guard.ts
@@ -12,10 +12,10 @@ export class AuthGuard implements CanActivate {
     next: ActivatedRouteSnapshot,
     state: RouterStateSnapshot): Observable<boolean> | Promise<boolean> | boolean {
       let username:string = localStorage.getItem('userName');
-      console.log('can activate',username);
       if(username && username.length>0){
         return true;
       }else{
+        console.log('can activate',username,state.url);
         this.router.navigate(['/login'], { queryParams: { returnUrl: state.url }});
         return false;
       }
